020e14f3b1a10e0499f7737e92648646f7e02740
VladMaS <vladmaslyuk.1304@gmail.com>
2025-03-28 10:49:14 +0200

    fix: model team

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Team extends Model
@@ -20,5 +21,9 @@ class Team extends Model
         return $this->morphOne(File::class, 'fileable');
     }
 
-    //TODO перенести звязок між юзером та табл team_user
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'team_user', 'team_id', 'user_id')
+            ->withTimestamps();
+    }
 }
